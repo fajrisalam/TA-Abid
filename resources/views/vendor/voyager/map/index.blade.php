45e474e5e2db9fd099290131f131100c724ca45b
fajrisalam <fajrisalam289@gmail.com>
2019-05-05 01:29:03 +0700

    menu baru

--- resources/views/vendor/voyager/map/index.blade.php
+++ resources/views/vendor/voyager/map/index.blade.php
@@ -6,7 +6,6 @@
         @foreach($data as $d)
             {{$d->nama_karyawan}} {{$d->arduino->nama_ruang}} {{$d->arduino->x}} {{$d->arduino->y}}<br>
         @endforeach
-        <a href="{{url('/admin')}}">Kembali</a>
         </div>
 @stop
 
@@ -27,8 +26,6 @@
     }
 
     function draw() {
-        // image(img, 0, 0);
-        // image(img, 0, 0, img.width / 2, img.height / 2);
         background(img_bg);
         @foreach($data as $d)
         image(img, {{$d->arduino->x}}, {{$d->arduino->y}}, 20, 20);
@@ -36,37 +33,3 @@
     }
 </script>
 @stop
-
-<!-- <script src="{{ asset('/js/p5/p5.js') }}"></script>
-<script>
-    var img_bg;
-    var img;
-    function preload(){
-        img_bg = loadImage('{{asset('/img/petaits.jpg')}}');
-        img = loadImage('{{asset('/img/icon4.png')}}');
-    }
-    function setup() {
-        var canvas = createCanvas(555, 700);
-        var bunder = ellipse(50, 50, 80, 80);
-
-        canvas.parent('#gambar');
-        textAlign(CENTER, CENTER);
-    }
-
-    function draw() {
-        // image(img, 0, 0);
-        // image(img, 0, 0, img.width / 2, img.height / 2);
-        background(img_bg);
-        @foreach($data as $d)
-        image(img, {{$d->arduino->x}}, {{$d->arduino->y}}, 20, 20);
-        @endforeach
-    }
-</script> -->
-<!-- 
-<body>
-    <div id="gambar"></div>
-    @foreach($data as $d)
-        {{$d->nama_karyawan}} {{$d->arduino->nama_ruang}} {{$d->arduino->x}} {{$d->arduino->y}}<br>
-    @endforeach
-    <a href="{{url('/admin')}}">Kembali</a>
-</body> -->
